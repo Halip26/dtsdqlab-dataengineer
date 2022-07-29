3f2697477ccc25ef6dfe1e4ddc9aa65430d96b55
Halip26 <halipuddin.angko@gmail.com>
2022-07-29 23:29:57 +0800

    Second commit

--- Python for Data Professional Beginner/Part 1/35.for-loops-2.py
+++ Python for Data Professional Beginner/Part 1/35.for-loops-2.py
@@ -1,8 +1,22 @@
 list_tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
-total_tagihan = 0
-for tagihan in list_tagihan :
-    if tagihan < 0 :
-        print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+
+# For loops with break
+print("For loops with break")
+total_tagihan_break = 0
+for tagihan in list_tagihan:
+    if tagihan < 0:
+        print("Terdapat angka minus dalam tagihan, perhitungan dihentikan!")
         break
-    total_tagihan += tagihan
-print(total_tagihan)
+    total_tagihan_break += tagihan
+print("Total tagihan %d." % total_tagihan_break)
+print()
+
+# For loops with continue
+print("For loops with continue")
+total_tagihan_continue = 0
+for tagihan in list_tagihan:
+    if tagihan < 0:
+        print("Terdapat angka minus dalam tagihan, tagihan %d dilewati!" % tagihan)
+        continue
+    total_tagihan_continue += tagihan
+print("Total tagihan %d." % total_tagihan_continue)
