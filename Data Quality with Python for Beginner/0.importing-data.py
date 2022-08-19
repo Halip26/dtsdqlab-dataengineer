a60ae98a868daa0e8606f4ab5ca7233c6633946b
Halip26 <halipuddin.angko@gmail.com>
2022-08-19 22:04:12 +0800

    Second commit

--- Data Quality with Python for Beginner/0.importing-data.py
+++ Data Quality with Python for Beginner/0.importing-data.py
@@ -2,4 +2,4 @@ import pandas as pd
 import numpy as np
 import io
 import pandas_profiling
-retail_raw = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced_data_quality.csv')
+retail_raw = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced_data_quality.csv')
--- Data Quality with Python for Beginner/1.datatype-inspection.py
+++ Data Quality with Python for Beginner/1.datatype-inspection.py
@@ -1,2 +1,8 @@
+import pandas as pd
+import numpy as np
+import io
+import pandas_profiling
+retail_raw = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced_data_quality.csv')
+
 # Cetak tipe data di setiap kolom retail_raw
 print(retail_raw.dtypes)
