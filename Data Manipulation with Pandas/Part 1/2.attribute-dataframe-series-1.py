974728186ac6729a84f210b36a7c3d80d529ed21
Halip26 <halipuddin.angko@gmail.com>
2022-08-24 10:59:04 +0800

    Second commit

--- Data Manipulation with Pandas/Part 1/2.attribute-dataframe-series-1.py
+++ Data Manipulation with Pandas/Part 1/2.attribute-dataframe-series-1.py
@@ -6,8 +6,8 @@ matrix_list = pd.DataFrame([[1,2,3],
 				            ['a','b','c'],
 				            [3,4,5],
 				            ['d',4,6]])
-# [1] attribute .info()
-print("[1] attribute .info()")
+# [1] method .info()
+print("[1] method .info()")
 print(matrix_list.info())
 # [2] attribute .shape
 print("\n[2] attribute .shape")
@@ -18,6 +18,6 @@ print("\n[3] attribute .dtypes")
 print("    Tipe data number_list:", number_list.dtypes)
 print("    Tipe data matrix_list:", matrix_list.dtypes)
 # [4] attribute .astype()
-print("\n[4] attribute .astype()")
+print("\n[4] method .astype()")
 print("    Konversi number_list ke str:", number_list.astype("str"))
 print("    Konversi matrix_list ke str:", matrix_list.astype("str"))
