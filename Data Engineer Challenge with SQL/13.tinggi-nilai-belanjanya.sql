3b3f7a3c8e1600c11272e27c52961cbf956d49ce
Halip26 <halipuddin.angko@gmail.com>
2022-08-29 06:36:37 +0800

    Second commit

--- Data Engineer Challenge with SQL/13.tinggi-nilai-belanjanya.sql
+++ Data Engineer Challenge with SQL/13.tinggi-nilai-belanjanya.sql
@@ -1,13 +1,1 @@
-SELECT
- ms_pelanggan.kode_pelanggan,
- ms_pelanggan.nama_pelanggan,
- SUM(tr_penjualan_detail.qty*tr_penjualan_detail.harga_satuan) AS total_harga
-FROM ms_pelanggan
-INNER JOIN tr_penjualan
-ON ms_pelanggan.kode_pelanggan = tr_penjualan.kode_pelanggan
-INNER JOIN tr_penjualan_detail
-ON tr_penjualan.kode_transaksi = tr_penjualan_detail.kode_transaksi
-GROUP BY 
- ms_pelanggan.kode_pelanggan, 
- ms_pelanggan.nama_pelanggan
-ORDER BY total_harga DESC LIMIT 1;
+SELECT ms_pelanggan.kode_pelanggan, ms_pelanggan.nama_pelanggan, SUM(tr_penjualan_detail.qty*tr_penjualan_detail.harga_satuan) AS total_harga FROM ms_pelanggan INNER JOIN tr_penjualan ON ms_pelanggan.kode_pelanggan = tr_penjualan.kode_pelanggan INNER JOIN tr_penjualan_detail ON tr_penjualan.kode_transaksi = tr_penjualan_detail.kode_transaksi GROUP BY ms_pelanggan.kode_pelanggan, ms_pelanggan.nama_pelanggan ORDER BY total_harga DESC LIMIT 1;
